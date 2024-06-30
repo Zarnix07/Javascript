e0ccf1e58b4c9446111c002f9759cfde17e5d4a7
Hitesh Rana <ranahitesh2006@gmail.com>
2024-06-30 02:06:27 +0000

    Datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (6+6) // we are using nodejs, not browser
+
+let name = "Hitesh"
+let age = 18 
+let isLoggedIn = true
+
+// numebr => 2 to power 52
+// string => "xyz"
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+console.log(typeof "Hitesh"); // string
+console.log(typeof null); // object
+console.log(typeof undefined);  // undefined
